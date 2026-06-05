0c3b4f3ef698638dc79ecdedd8c3ab9563e2c44d
inbarasu <b.inba.ips444@gmail.com>
2026-06-05 10:08:19 +0530

    Add user profile fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -16,7 +13,37 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=ROLE_CHOICES,
-        default='member'
+        null=True,
+        blank=True
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        null=True,
+        blank=True
+    )
+
+    dob = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    age = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    salary = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    address = models.TextField(
+        null=True,
+        blank=True
     )
 
     def __str__(self):
