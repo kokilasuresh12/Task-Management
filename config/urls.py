161636d046b793c10915c00d85b0b6cbd6a6be52
kokilasuresh12 <kokilas0307@gmail.com>
2026-06-15 12:55:31 +0530

    Fix root URL

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({'message': 'Task Management API is running!'})
 
 urlpatterns = [
+    path('', home),                              # ← fixes root 404
     path('admin/', admin.site.urls),
-
     path('api/', include('accounts.api_urls')),
 ]
 
